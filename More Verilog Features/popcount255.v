02765e547c85a132612ef79360bb99304061e43a
Gowtham Kumar S <journeytowardssilicon@gmail.com>
2026-07-13 22:04:34 +0530

    Add files via upload

---
+++ More Verilog Features/popcount255.v
@@ -0,0 +1,23 @@
+module top_module( 
+    input [254:0] in,
+    output [7:0] out );
+    
+    integer i; 
+    
+    always @ (*) begin
+        
+       out = 8'd0; 
+        
+        for (i=0; i<255; i=i+1) 
+            if (in[i] == 1) begin
+                out = out + 1; 
+            end
+        
+        	else begin
+                out = out; 
+            end
+        
+    end
+
+endmodule
+
