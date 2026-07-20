71c8d356206dff8f038b5c17453869fbe66e84fa
Gowtham Kumar S <journeytowardssilicon@gmail.com>
2026-07-20 09:19:19 +0530

    Create dff_gate.v

---
+++ Circuits/Sequential Circuits/ Latches and Flipflops/dff_gate.v
@@ -0,0 +1,14 @@
+module top_module (
+    input clk,
+    input in, 
+    output out);
+    
+    wire x;
+    
+    always @(posedge clk) begin
+        
+        out <= in ^ out; 
+        
+    end
+
+endmodule
