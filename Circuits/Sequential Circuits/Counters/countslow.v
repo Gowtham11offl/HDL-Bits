178527632a8f822e82b4cded697c1ac284e55f59
Gowtham Kumar S <journeytowardssilicon@gmail.com>
2026-07-23 21:40:51 +0530

    Create countslow.v

---
+++ Circuits/Sequential Circuits/Counters/countslow.v
@@ -0,0 +1,20 @@
+module top_module (
+    input clk,
+    input slowena,
+    input reset,
+    output [3:0] q);
+    
+    always @ (posedge clk) begin 
+        
+        if(reset)
+            q <= 4'd0; 
+        else
+            if(slowena && q == 4'd9)
+                q <= 4'd0; 
+        else if (slowena)
+            	q <= q + 1'b1; 
+                
+        
+    end
+
+endmodule
