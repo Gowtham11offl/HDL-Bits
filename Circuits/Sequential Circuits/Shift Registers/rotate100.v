cca8f17908d6ed7eb57e7dc86f802d607f50777f
Gowtham Kumar S <journeytowardssilicon@gmail.com>
2026-07-30 09:59:30 +0530

    Create rotate100.v

---
+++ Circuits/Sequential Circuits/Shift Registers/rotate100.v
@@ -0,0 +1,25 @@
+module top_module(
+    input clk,
+    input load,
+    input [1:0] ena,
+    input [99:0] data,
+    output reg [99:0] q); 
+    
+    always @(posedge clk) begin 
+        
+     
+        
+        casex ({load,ena}) 
+             
+            3'b001 : q <= {q[0], q[99:1]}; 
+            3'b010 : q <= {q[98:0], q[99]};
+            3'b1xx : q <= data;
+            
+            default : begin 
+                q <= q; 
+            end
+        endcase
+        
+    end
+
+endmodule
