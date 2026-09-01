892946cd01bb5f0ebf01bb994cb72493b682ea6e
Gowtham Kumar S <journeytowardssilicon@gmail.com>
2026-09-01 19:52:08 +0530

    Create 02. rule110.v

---
+++ Circuits/Sequential Circuits/More Circuits/02. rule110.v
@@ -0,0 +1,23 @@
+module top_module(
+    input clk,
+    input load,
+    input [511:0] data,
+    output [511:0] q
+); 
+    integer i;
+    always @(posedge clk)begin
+        if(load)
+            q<=data;
+        else begin
+            for(i=0;i<512;i=i+1)begin
+                if(i==0)
+                    q[i]<=((q[i]^1'b0)|(~q[i+1]&q[i]));
+                else if(i==511)
+                    q[i]<=((q[i]^q[i-1])|(1'b1&q[i]));
+                else
+                    q[i]<=((q[i]^q[i-1])|(~q[i+1]&q[i])); 
+            end  
+        end
+    end
+
+endmodule
